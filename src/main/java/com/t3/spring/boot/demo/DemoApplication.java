bff9676324b8349ea42988e472d0b28f8739c23a
tttrinh <trinhtoantrung@gmail.com>
2017-03-29 15:05:31 +0700

    Init Spring Boot: jpa + h2 database + config

---
+++ src/main/java/com/t3/spring/boot/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.t3.spring.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
